8c8e25583683a0f29b3c16609ac7a4d4e897fef1
k4im <contato.vitorsantos@hotmail.com>
2023-08-04 16:02:07 -0300

    Adicionado Environment

--- Program.cs
+++ Program.cs
@@ -3,10 +3,9 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 IConfiguration configuration = new ConfigurationBuilder()
-                            .AddJsonFile("ocelot.json")
+                            .AddJsonFile($"ocelot.{builder.Configuration["ASPNETCORE_ENVIRONMENT"]}.json")
                             .Build();
 builder.Services.AddOcelot(configuration);
-// Add services to the container.
 
 var app = builder.Build();
 app.UseOcelot().Wait();
