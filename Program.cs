2292d1309b2e35dfec67ddd9ffa2f351c33dd40e
k4im <contato.vitorsantos@hotmail.com>
2023-08-08 10:18:29 -0300

    Adicionado correlation ID

--- Program.cs
+++ Program.cs
@@ -2,9 +2,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOcelotConfigurations(builder.Configuration);
 builder.Services.AddJWtConfigurations(builder.Configuration);
 builder.Services.AddHealthChecks();
+builder.Services.AddCors();
 
 var app = builder.Build();
 app.UseOcelot().Wait();
 app.MapGet("/", () => "Ocelot esta funcionando");
 app.MapHealthChecks("/health");
+app.UseCors(
+    x => {
+        x.AllowAnyOrigin();
+        x.AllowAnyMethod();
+    }
+);
 app.Run();
