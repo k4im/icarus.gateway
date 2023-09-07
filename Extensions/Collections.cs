36db25f6da237f1b27a06268f335cec318c34922
k4im <contato.vitorsantos@hotmail.com>
2023-09-07 14:01:52 -0300

    Removido env

--- Extensions/Collections.cs
+++ Extensions/Collections.cs
@@ -5,7 +5,7 @@ public static class Collections
     public static IServiceCollection AddOcelotConfigurations(this IServiceCollection services, IConfiguration config)
     {
         var configuration = new ConfigurationBuilder()
-        .AddJsonFile($"ocelot.{config["ASPNETCORE_ENVIRONMENT"]}.json")
+        .AddJsonFile($"ocelot.json")
         .Build();
         services.AddOcelot(configuration);
         return services;
